7253d8d99d41c3f87c0835ac255b9f778a2b3439
Jesus Marquez <jesmarquezacevedo@yahoo.es>
2017-03-18 23:06:58 -0500

    implementacion auth

--- test/client-test.js
+++ test/client-test.js
@@ -131,3 +131,22 @@ test('getUser', async t => {
 
   t.deepEqual(result, user)
 })
+
+test('auth', async t => {
+  const client = t.context.client
+
+  let credentials = {
+    username: 'freddier',
+    password: 'pl4tzi'
+  }
+
+  let token = 'xxx-xxx-xxx'
+
+  nock(options.endpoints.auth)
+    .post('/', credentials)
+    .reply(200, token)
+
+  let result = await client.auth(credentials.username, credentials.password)
+
+  t.deepEqual(result, token)
+})
